3a7b5f187403a80cab191d359040ebfc75f00266
Maxim <mdparshin@yandex.ru>
2016-10-24 13:42:07 +0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
-#include "Pupils.h"
+#include "Pupil.h"
+using std::string;
 
 int main()
 {
-    Pupils pupil;
+    Pupil pupil;
 
     std::string name;
     std::string second_name;
 
+    std::cin>>name;
    
-    std::cout << "Name: ";//Имя с клавиатуры
-    getline(std::cin, name);
+    //std::cout << "Name: ";//Имя с клавиатуры
+    //getline(std::cin, name);
 
     
-    std::cout << "Second name: ";//Фамилия с клавиатуры
-    getline(std::cin, second_name);
+    // std::cout << "Second name: ";//Фамилия с клавиатуры
+	//getline(std::cin, second_name);
 
-    pupil.set_name(name);//Созранение имени и фамилии
-    pupil.set_second_name(second_name);
+	std::cin>>second_name;
+    pupil.setName(name);//Созранение имени и фамилии
+    pupil.setSecondName(second_name);
 
     int scores[5];//Оценки
     int sum = 0;//Сумма оценок 
@@ -30,12 +33,12 @@ int main()
     }
 
     
-    pupil.set_scores(scores);//Сохранение оценок 
+    pupil.setScores(scores);//Сохранение оценок 
     float medium_mark = sum / 5.0;// Подсчет средней оценки
-    pupil.set_medium_mark(medium_mark);// Средняя оценка
-    std::cout << "Medium mark of " << pupil.get_name() << " "
-         << pupil.get_second_name() << " is "
-         << pupil.get_medium_mark() << std::endl;//Итог данных по студенту
+    pupil.setMediumMark(medium_mark);// Средняя оценка
+    std::cout << "Medium mark of " << pupil.getName() << " "
+         << pupil.getSecondName() << " is "
+         << pupil.getMediumMark() << std::endl;//Итог данных по студенту
 
     return 0;
 }
