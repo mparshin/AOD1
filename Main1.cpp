350cd56cb1246a66cf2fdea73292bae6c2ef4561
Maxim <mdparshin@yandex.ru>
2016-10-29 15:48:45 +0300

    Add files via upload
    Некоторые замечания были исправлены, к сожалению все не получилось исправить, не понимаю как.

---
+++ Main1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "Pupil1.h"
+
+int main()
+{
+    Pupil pupil;
+
+    std::string name;
+    std::string second_name;
+
+   
+    //std::cout << "Name: ";//Имя с клавиатуры
+    //getline(std::cin, name);
+
+    //std::cout << "Second name: ";//Фамилия с клавиатуры
+    //getline(std::cin, second_name);
+	std::cin>>name;
+	std::cin>>second_name;
+	
+    pupil.setName(name);//Созранение имени и фамилии
+    pupil.setSecondName(second_name);
+
+    int scores[5];//Оценки
+    int sum = 0;//Сумма оценок 
+
+    // Ввод оценок
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Score " << i+1 << ": ";
+        std::cin >> scores[i];
+        sum += scores[i];// Сложение 
+    }
+
+	name = pupil_name;
+	return name;
+	second_name = pupil_second_name;
+	return second_name;
+	for (int i = 0; i < 5; ++i) {
+         scores[i] = pupil_scores[i];}
+		  medium_mark = mark;
+		   return medium_mark;   
+    pupil.setScores(scores);//Сохранение оценок 
+    float medium_mark = sum / 5.0;// Подсчет средней оценки
+    pupil.setMediumMark(medium_mark);// Средняя оценка
+    std::cout << "Medium mark of " << pupil.get_name() << " "
+         << pupil.getSecondName() << " is "
+         << pupil.getMediumMark() << std::endl;//Итог данных по студенту
+
+    return 0;
+}
