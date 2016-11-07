f04e8120875e63cccd2c6cc517c8c8a8c55b2411
Maxim <mdparshin@yandex.ru>
2016-11-07 13:49:29 +0300

    Add files via upload
    upgrade 07.11.16

--- Main1.cpp
+++ Main1.cpp
@@ -3,38 +3,38 @@
 
 int main()
 {
-    Pupil pupil;
+    Pupil1 pupil;
 
     std::string name;
     std::string second_name;
 
    
-    //std::cout << "Name: ";//Г€Г¬Гї Г± ГЄГ«Г ГўГЁГ ГІГіГ°Г»
+    //std::cout << "Name: ";//Имя с клавиатуры
     //getline(std::cin, name);
 
-    //std::cout << "Second name: ";//Г”Г Г¬ГЁГ«ГЁГї Г± ГЄГ«Г ГўГЁГ ГІГіГ°Г»
+    //std::cout << "Second name: ";//Фамилия с клавиатуры
     //getline(std::cin, second_name);
 	std::cin>>name;
 	std::cin>>second_name;
 	
-    pupil.setName(name);//Г‘Г®Г§Г°Г Г­ГҐГ­ГЁГҐ ГЁГ¬ГҐГ­ГЁ ГЁ ГґГ Г¬ГЁГ«ГЁГЁ
+    pupil.setName(name);//Созранение имени и фамилии
     pupil.setSecondName(second_name);
 
-    int scores[5];//ГЋГ¶ГҐГ­ГЄГЁ
-    int sum = 0;//Г‘ГіГ¬Г¬Г  Г®Г¶ГҐГ­Г®ГЄ 
+    int scores[5];//Оценки
+    int sum = 0;//Сумма оценок 
 
-    // Г‚ГўГ®Г¤ Г®Г¶ГҐГ­Г®ГЄ
+    // Ввод оценок
     for (int i = 0; i < 5; ++i) {
         std::cout << "Score " << i+1 << ": ";
         std::cin >> scores[i];
-        sum += scores[i];// Г‘Г«Г®Г¦ГҐГ­ГЁГҐ 
+        sum += scores[i];// Сложение 
     }   
-    pupil.setScores(scores);//Г‘Г®ГµГ°Г Г­ГҐГ­ГЁГҐ Г®Г¶ГҐГ­Г®ГЄ 
-    float medium_mark = sum / 5.0;// ГЏГ®Г¤Г±Г·ГҐГІ Г±Г°ГҐГ¤Г­ГҐГ© Г®Г¶ГҐГ­ГЄГЁ
-    pupil.setMediumMark(medium_mark);// Г‘Г°ГҐГ¤Г­ГїГї Г®Г¶ГҐГ­ГЄГ 
+    pupil.setScores(scores);//Сохранение оценок 
+    float medium_mark = sum / 5.0;// Подсчет средней оценки
+    pupil.setMediumMark(medium_mark);// Средняя оценка
     std::cout << "Medium mark of " << pupil.getName() << " "
          << pupil.getSecondName() << " is "
-         << pupil.getMediumMark() << std::endl;//Г€ГІГ®ГЈ Г¤Г Г­Г­Г»Гµ ГЇГ® Г±ГІГіГ¤ГҐГ­ГІГі
+         << pupil.getMediumMark() << std::endl;//Итог данных по студенту
 
     return 0;
 }
